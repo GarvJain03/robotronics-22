e00d78d0e96307fe5b867ee946c3765b10722de2
Mehul Srivastava <mehulsrivastava1206@gmail.com>
2022-10-29 18:11:47 +0530

    add: new sections to landing page

---
+++ components/Home/BestApp.tsx
@@ -0,0 +1,11 @@
+/* eslint-disable @next/next/no-img-element */
+// @ts-nocheck
+import React from 'react'
+
+const BestApp = () => {
+  return (
+    <div>BestApp</div>
+  )
+}
+
+export default BestApp;
---
+++ components/Home/Features.tsx
@@ -0,0 +1,11 @@
+/* eslint-disable @next/next/no-img-element */
+// @ts-nocheck
+import React from 'react'
+
+const Features = () => {
+  return (
+    <div>Features</div>
+  )
+}
+
+export default Features;
